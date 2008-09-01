a0101df0e542bd6e8f0013d2873b43f527427af9
Max Horvath <info@maxhorvath.com>
2008-09-01 10:53:36 +0000

    #24: Added simple example on how to use PHPTypeSafe ...
    git-svn-id: https://dev.maxhorvath.com/svn/phptypesafe/trunk@27 28807a0e-477c-4ebd-8253-2988b8b1ce4f

---
+++ src/examples/php/com/maxhorvath/phptypesafe/SimpleExample.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * PHPTypeSafe
+ *
+ * Copyright (C) 2008  Max Horvath <info@maxhorvath.com>.
+ *
+ * This file is part of PHPTypeSafe.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   PHP
+ * @package    com::maxhorvath::phptypesafe
+ * @subpackage examples
+ * @author     Max Horvath <info@maxhorvath.com>
+ * @copyright  2008 Max Horvath <info@maxhorvath.com>
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ * @version    SVN: $Id$
+ * @link       http://www.maxhorvath.com/
+ * @since      File available since release 1.0.0
+ */
+
+/**
+ * Define namespace
+ */
+namespace com::maxhorvath::phptypesafe;
+
+/**
+ * Define includes
+ */
+require_once 'com.maxhorvath.phptypesafe.phar';
+
+/**
+ * Test class.
+ *
+ * @category   PHP
+ * @package    com::maxhorvath::phptypesafe
+ * @subpackage examples
+ * @author     Max Horvath <info@maxhorvath.com>
+ * @copyright  2008 Max Horvath <info@maxhorvath.com>
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ * @version    Release: 1.0.0
+ * @link       http://www.maxhorvath.com/
+ * @since      Class available since release 1.0.0
+ */
+class Foo
+{
+    public static function bar(string $msg, int $counter)
+    {
+        echo $counter . '. run: ' . $msg . "\n\n";
+    }
+}
+
+$postCount = 0;
+
+Foo::bar('test', ++$postCount);
+
+echo "The next run will result in an an ErrorException: \n\n";
+
+Foo::bar(false, ++$postCount);
+
+?>
